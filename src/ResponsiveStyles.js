eeb6c19a4abc8a691ddd0253cd7b07e89c6de980
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2023-11-17 15:17:05 -0500

    update

--- src/ResponsiveStyles.js
+++ src/ResponsiveStyles.js
@@ -9,6 +9,9 @@ class ResponsiveStyles {
             this.linksSvgSpan = {verticalAlign: 'bottom', display: 'inline', padding: '0', margin: '0', marginRight: '10px', marginLeft: '10px'};
             this.linksFirstDot = {fontSize: '25px', display: 'inline', padding: '0', margin: '0', color: 'white', fontWeight: 'bold', marginLeft: '10px', marginRight: '10px'};
             this.linksSubsequentDots = {fontSize: '25px', display: 'inline', padding: '0', margin: '0', color: 'white', fontWeight: 'bold', marginLeft: '10px'};
+            this.linksSection = {textDecoration: 'none'};
+            this.linksSvgSection = {};
+            this.linksSvgSize = "28";
             
             this.sectionTitle = { paddingLeft: '6%', margin: '0', paddingTop: '3%', fontWeight: 'bold', color: 'black', fontSize: '54px'};
             
@@ -17,12 +20,16 @@ class ResponsiveStyles {
             this.skillsRow = {paddingLeft: '12%', paddingRight: '3%'};
         } else if (xl) { // >= 1200px, Mobile site
             console.log('xl');
-            this.name = {marginTop: '0', marginBottom: '8px', color: 'white', fontWeight: '500', fontSize: '82px', textAlign: 'center'};
-            this.links = {verticalAlign: 'middle', display: 'inline-block', padding: '0', margin: '0'};
-            this.linksParagraph = {fontSize: '28px', padding: '0', margin: '0', color: 'white', fontWeight: 'bold'};
-            this.linksSvgSpan = {verticalAlign: 'bottom', display: 'inline-block', padding: '0', margin: '0', marginRight: '10px', marginLeft: '10px'};
-            this.linksFirstDot = {fontSize: '28px', padding: '0', margin: '0', color: 'white', fontWeight: 'bold', marginLeft: '10px', marginRight: '10px'};
-            this.linksSubsequentDots = {fontSize: '28px', padding: '0', margin: '0', color: 'white', fontWeight: 'bold', marginLeft: '10px'};
+            this.name = {marginTop: '0', marginBottom: '8px', color: 'white', fontWeight: '500', fontSize: '135px', textAlign: 'center'};
+            this.links = {verticalAlign: 'middle', display: 'inline', padding: '0', margin: '0'};
+            this.linksParagraph = {fontSize: '35px', display: 'inline', padding: '0', margin: '0', color: 'white', fontWeight: 'bold'};
+            this.linksSvgSpan = {verticalAlign: 'bottom', display: 'inline', padding: '0', margin: '0', marginRight: '10px', marginLeft: '10px'};
+            this.linksFirstDot = {display: 'none'};
+            this.linksSubsequentDots = {display: 'none'};
+            this.linksSection = {textDecoration: 'none', display: 'block', textAlign: 'center', padding: '5px'};
+            this.linksSvgSection = {display: 'block', textAlign: 'center', padding: '5px'};
+            this.linksSvgSize = "38";
+            
             this.skills = { paddingTop: '3%', textAlign: 'center', fontSize: '28px', fontWeight: 'bold'};
             this.sectionTitle = { margin: '0', paddingTop: '3%', fontWeight: 'bold', color: 'black', fontSize: '54px'};
             this.skillsTitle = { margin: '0', fontSize: '36px', paddingTop: '3%', paddingLeft: '3%', fontWeight: 'bold', color: 'black'};
--- src/components/Header.js
+++ src/components/Header.js
@@ -13,44 +13,48 @@ function Header(props) {
                     <div style={{paddingLeft: '6%'}}>
                         <Title level={1} style={responsiveStyles.name}>Matthew Godin</Title>
                         <span>
-                            <Link to='mailto:matthew.godin.software.engineer@gmail.com' style={{textDecoration: 'none'}}>
-                                <span style={responsiveStyles.linksStyles}>
+                            <Link to='mailto:matthew.godin.software.engineer@gmail.com' style={responsiveStyles.linksSection}>
+                                <span style={responsiveStyles.links}>
                                     <Paragraph style={responsiveStyles.linksParagraph}>matthew.godin.software.engineer@gmail.com</Paragraph>
                                 </span>
                             </Link>
-                            <span style={responsiveStyles.linksStyles}>
+                            <span style={responsiveStyles.links}>
                                 <Paragraph style={responsiveStyles.linksFirstDot}>•</Paragraph>
                             </span>
-                            <Link to='tel:5196350965' style={{textDecoration: 'none'}}>
-                                <span style={responsiveStyles.linksStyles}>
+                            <Link to='tel:5196350965' style={responsiveStyles.linksSection}>
+                                <span style={responsiveStyles.links}>
                                     <Paragraph style={responsiveStyles.linksParagraph}>(519) 635-0965</Paragraph>
                                 </span>
                             </Link>
-                            <span style={responsiveStyles.linksStyles}>
+                            <span style={responsiveStyles.links}>
                                 <Paragraph style={responsiveStyles.linksSubsequentDots}>•</Paragraph>
                             </span>
-                            <span style={responsiveStyles.linksSvgSpan}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
-                                    <path style={{fill: 'white'}} d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
-                                </svg>
-                            </span>
-                            <Link to='https://www.linkedin.com/in/matthew-godin-software-engineer/' style={{textDecoration: 'none'}}>
-                                <span style={responsiveStyles.linksStyles}>
-                                    <Paragraph style={responsiveStyles.linksParagraph}>matthew-godin-software-engineer</Paragraph>
+                            <span style={responsiveStyles.linksSvgSection}>
+                                <span style={responsiveStyles.linksSvgSpan}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width={responsiveStyles.linksSvgSize} height={responsiveStyles.linksSvgSize} viewBox="0 0 24 24">
+                                        <path style={{fill: 'white'}} d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                                    </svg>
                                 </span>
-                            </Link>
-                            <span style={responsiveStyles.linksStyles}>
+                                <Link to='https://www.linkedin.com/in/matthew-godin-software-engineer/' style={{textDecoration: 'none'}}>
+                                    <span style={responsiveStyles.links}>
+                                        <Paragraph style={responsiveStyles.linksParagraph}>matthew-godin-software-engineer</Paragraph>
+                                    </span>
+                                </Link>
+                            </span>
+                            <span style={responsiveStyles.links}>
                                 <Paragraph style={responsiveStyles.linksSubsequentDots}>•</Paragraph>
                             </span>
-                            <span style={responsiveStyles.linksSvgSpan}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24">
-                                    <path style={{fill: 'white'}} d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
-                                </svg>
-                            </span>
-                            <span style={responsiveStyles.linksStyles}>
-                                <Link to="https://github.com/matthew-godin" style={{textDecoration: 'none'}}>
-                                    <Paragraph style={responsiveStyles.linksParagraph}>matthew-godin</Paragraph>
-                                </Link>
+                            <span style={responsiveStyles.linksSvgSection}>
+                                <span style={responsiveStyles.linksSvgSpan}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width={responsiveStyles.linksSvgSize} height={responsiveStyles.linksSvgSize} viewBox="0 0 24 24">
+                                        <path style={{fill: 'white'}} d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                                    </svg>
+                                </span>
+                                <span style={responsiveStyles.links}>
+                                    <Link to="https://github.com/matthew-godin" style={{textDecoration: 'none'}}>
+                                        <Paragraph style={responsiveStyles.linksParagraph}>matthew-godin</Paragraph>
+                                    </Link>
+                                </span>
                             </span>
                         </span>
                     </div>
