bc56034555e5f28ab464f93a69799a55d4f9372d
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2023-11-18 05:46:21 -0500

    skills three columsn version

--- src/ResponsiveStyles.js
+++ src/ResponsiveStyles.js
@@ -47,8 +47,8 @@ class ResponsiveStyles {
             this.sectionsMobileSite = 'none';
             this.displayMobileSite = 'block';
             
-            this.skillsTitle = { fontSize: '32px', fontWeight: 'bold', color: 'black', textAlign: 'center' };
-            this.skills = { textAlign: 'center', fontSize: '28px', fontWeight: 'bold', color: 'black'};
+            this.skillsTitle = { fontSize: '30px', fontWeight: 'bold', color: 'black', textAlign: 'center' };
+            this.skills = { textAlign: 'center', fontSize: '26px', fontWeight: 'bold', color: 'black'};
             this.skillsVh = '10vh';
 
             this.jobTitleMobile = { textAlign: 'center', fontSize: '24px', fontWeight: 'bold'};
@@ -71,15 +71,19 @@ class ResponsiveStyles {
             this.headerChevronMarginTop = "10%";
             this.headerLink = '#programming-languages';
             
-            this.sectionTitle = { paddingLeft: '6%', margin: '0', paddingTop: '3%', fontWeight: 'bold', color: 'black', fontSize: '54px'};
+            this.sectionTitle = { margin: '0', fontSize: '37.5px', paddingTop: '3.5%', fontWeight: 'bold', color: 'black', textAlign: 'center' };
             this.chevronSize = '45px';
             this.sectionsMobileSite = 'none';
             this.displayMobileSite = 'block';
             
-            this.skills = { textAlign: 'center', fontSize: '28px', fontWeight: 'bold', color: 'black'};
-            this.skillsTitle = { margin: '0', fontSize: '36px', paddingLeft: '9%', fontWeight: 'bold', color: 'black'};
-            this.skillsRow = {paddingLeft: '12%', paddingRight: '3%'};
+            this.skillsTitle = { fontSize: '26px', fontWeight: 'bold', color: 'black', textAlign: 'center' };
+            this.skills = { textAlign: 'center', fontSize: '22px', fontWeight: 'bold', color: 'black'};
             this.skillsVh = '10vh';
+
+            this.jobTitleMobile = { textAlign: 'center', fontSize: '24px', fontWeight: 'bold'};
+            this.jobDateMobile = { textAlign: 'center', fontSize: '18px', fontWeight: 'bold'};
+
+            this.projectDateMobile = { textAlign: 'center', fontSize: '21px', fontWeight: 'bold'};
         } else if (md) { // >= 768px
             console.log('md');
             this.name = {marginTop: '0', marginBottom: '8px', color: 'white', fontWeight: '500', fontSize: '85px', textAlign: 'center'};
@@ -96,15 +100,19 @@ class ResponsiveStyles {
             this.headerChevronMarginTop = "10%";
             this.headerLink = '#programming-languages';
             
-            this.sectionTitle = { paddingLeft: '6%', margin: '0', paddingTop: '3%', fontWeight: 'bold', color: 'black', fontSize: '54px'};
+            this.sectionTitle = { margin: '0', fontSize: '35px', paddingTop: '3.5%', fontWeight: 'bold', color: 'black', textAlign: 'center' };
             this.chevronSize = '42.5px';
             this.sectionsMobileSite = 'none';
             this.displayMobileSite = 'block';
             
-            this.skills = { textAlign: 'center', fontSize: '28px', fontWeight: 'bold'};
-            this.skillsTitle = { margin: '0', fontSize: '36px', paddingLeft: '9%', fontWeight: 'bold', color: 'black'};
-            this.skillsRow = {paddingLeft: '12%', paddingRight: '3%'};
+            this.skillsTitle = { fontSize: '20px', fontWeight: 'bold', color: 'black', textAlign: 'center' };
+            this.skills = { textAlign: 'center', fontSize: '16px', fontWeight: 'bold', color: 'black'};
             this.skillsVh = '10vh';
+
+            this.jobTitleMobile = { textAlign: 'center', fontSize: '24px', fontWeight: 'bold'};
+            this.jobDateMobile = { textAlign: 'center', fontSize: '18px', fontWeight: 'bold'};
+
+            this.projectDateMobile = { textAlign: 'center', fontSize: '21px', fontWeight: 'bold'};
         } else if (sm) { // >= 576px
             console.log('sm');
             this.name = {marginTop: '0', marginBottom: '8px', color: 'white', fontWeight: '500', fontSize: '65px', textAlign: 'center'};
@@ -121,15 +129,19 @@ class ResponsiveStyles {
             this.headerChevronMarginTop = "10%";
             this.headerLink = '#programming-languages';
             
-            this.sectionTitle = { paddingLeft: '6%', margin: '0', paddingTop: '3%', fontWeight: 'bold', color: 'black', fontSize: '54px'};
+            this.sectionTitle = { margin: '0', fontSize: '32.5px', paddingTop: '3.5%', fontWeight: 'bold', color: 'black', textAlign: 'center' };
             this.chevronSize = '40px';
             this.sectionsMobileSite = 'none';
             this.displayMobileSite = 'block';
             
-            this.skills = { textAlign: 'center', fontSize: '28px', fontWeight: 'bold'};
-            this.skillsTitle = { margin: '0', fontSize: '36px', paddingLeft: '9%', fontWeight: 'bold', color: 'black'};
-            this.skillsRow = {paddingLeft: '12%', paddingRight: '3%'};
+            this.skillsTitle = { fontSize: '14px', fontWeight: 'bold', color: 'black', textAlign: 'center' };
+            this.skills = { textAlign: 'center', fontSize: '10px', fontWeight: 'bold', color: 'black'};
             this.skillsVh = '10vh';
+
+            this.jobTitleMobile = { textAlign: 'center', fontSize: '24px', fontWeight: 'bold'};
+            this.jobDateMobile = { textAlign: 'center', fontSize: '18px', fontWeight: 'bold'};
+
+            this.projectDateMobile = { textAlign: 'center', fontSize: '21px', fontWeight: 'bold'};
         } else if (xs) { // < 576px
             console.log('xs');
             this.name = {marginTop: '0', marginBottom: '8px', color: 'white', fontWeight: '500', fontSize: '45px', textAlign: 'center'};
@@ -146,15 +158,19 @@ class ResponsiveStyles {
             this.headerChevronMarginTop = "10%";
             this.headerLink = '#programming-languages';
             
-            this.sectionTitle = { paddingLeft: '6%', margin: '0', paddingTop: '3%', fontWeight: 'bold', color: 'black', fontSize: '54px'};
+            this.sectionTitle = { margin: '0', fontSize: '30px', paddingTop: '3.5%', fontWeight: 'bold', color: 'black', textAlign: 'center' };
             this.chevronSize = '37.5px';
             this.sectionsMobileSite = 'none';
             this.displayMobileSite = 'block';
             
-            this.skills = { textAlign: 'center', fontSize: '28px', fontWeight: 'bold'};
-            this.skillsTitle = { margin: '0', fontSize: '36px', paddingLeft: '9%', fontWeight: 'bold', color: 'black'};
-            this.skillsRow = {paddingLeft: '12%', paddingRight: '3%'};
+            this.skillsTitle = { fontSize: '8px', fontWeight: 'bold', color: 'black', textAlign: 'center' };
+            this.skills = { textAlign: 'center', fontSize: '4px', fontWeight: 'bold', color: 'black'};
             this.skillsVh = '10vh';
+
+            this.jobTitleMobile = { textAlign: 'center', fontSize: '24px', fontWeight: 'bold'};
+            this.jobDateMobile = { textAlign: 'center', fontSize: '18px', fontWeight: 'bold'};
+
+            this.projectDateMobile = { textAlign: 'center', fontSize: '21px', fontWeight: 'bold'};
         }
     }
 }
