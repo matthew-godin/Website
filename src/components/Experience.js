a5622dc70cc3a10c232ff11fabf113dafc757d50
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2023-11-25 21:01:34 -0500

    update

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -70,7 +70,7 @@ function Experience(props) {
             <div style={{minHeight: '6vh', maxHeight: '6vh', position: 'relative', width: '100%', display: responsiveStyles.sectionsMobileSite}}>
                 <div style={{ paddingBottom: '2%', position: 'absolute', top: 0, width: '100%'}}>
                     <Row style={experienceSecondLineStyle}>
-                        <Col style={jobDescriptionStyle} xs={16}>Developed new features for the Dayforce application (Compensation Cycles and Reward Letter)</Col>
+                        <Col style={jobDescriptionStyle} xs={16}>Developed new features for the Dayforce application (Compensation Cycles and Reward Letters)</Col>
                         <Col xs={8} style={{textAlign: 'right', fontSize: '20px', fontWeight: jobSkillsWeight}}>C# • TypeScript • SQL • ASP.NET Core • React</Col>
                     </Row>
                 </div>
