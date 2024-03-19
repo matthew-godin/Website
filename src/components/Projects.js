52b08d64444a16619377d35b18adfc21732e4a57
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-03-18 21:47:51 -0400

    update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,7 +12,7 @@ function Projects(props) {
                 {PROJECTS.content.map((project, i) =>
                     <Project title={project.title} link={project.link} date={project.date}
                         description={project.description + " (" + project.mention + ")"}
-                        skills={project.skills} responsiveStyles={responsiveStyles} largeDescription={i === PROJECTS.content.length - 1} />
+                        skills={project.skills} responsiveStyles={responsiveStyles} largeDescription={i >= PROJECTS.content.length - 2} />
                 )}
             <div style={{minHeight: '12vh', maxHeight: '12vh', position: 'relative', width: '100%', display: responsiveStyles.sectionsMobileSite}}>
                 <div style={{ paddingBottom: '2%', position: 'absolute', bottom: 0, width: '100%'}} />
