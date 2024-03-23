b910d268defabbc768ba4719a87cc97766c750ee
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-03-23 16:01:00 -0400

    remove bottom spacing mobile

--- src/components/mobile/MobileProjects.js
+++ src/components/mobile/MobileProjects.js
@@ -4,9 +4,7 @@ import MobilePortraitProject from './MobilePortraitProject';
 import MobileLandscapeProject from './MobileLandscapeProject';
 import PROJECTS from '../../constants/content/Projects';
 
-const PORTRAIT_BOTTOM_SPACING = 25.25;
 const LANDSCAPE_TOP_SPACING = 11.5;
-const LANDSCAPE_BOTTOM_SPACING = 15;
 
 function MobileProjects(props) {
     const responsiveStyles = props.responsiveStyles;
@@ -18,7 +16,6 @@ function MobileProjects(props) {
                     <MobilePortraitProject link={project.link} title={project.title} description={project.headline}
                         additionalDescription={project.mentionHeadline} date={project.date} responsiveStyles={responsiveStyles} />
                 )}
-                <div style={getDivStyle(PORTRAIT_BOTTOM_SPACING, responsiveStyles.displayMobileSite)} />
             </>
         );
     } else {
@@ -30,7 +27,6 @@ function MobileProjects(props) {
                     <MobileLandscapeProject link={project.link} title={project.title + " - " + project.headline}
                         responsiveStyles={responsiveStyles} />
                 )}
-                <div style={getDivStyle(LANDSCAPE_BOTTOM_SPACING, responsiveStyles.displayMobileSite)} />
             </>
         );
     }
