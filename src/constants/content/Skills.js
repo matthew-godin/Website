5d901b7992de3772b22646f16cb48bc8a105e447
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2024-04-03 02:44:39 -0400

    update

--- src/constants/content/Skills.js
+++ src/constants/content/Skills.js
@@ -5,21 +5,22 @@ const SKILLS = {
         content: [
             { name: 'TypeScript', desktop: 3, mobile: 8 },
             { name: 'JavaScript', desktop: 3, mobile: 8 },
-            { name: 'C#', desktop: 2, mobile: 4 },
+            { name: 'C#', desktop: 1, mobile: 4 },
             { name: 'Java', desktop: 2, mobile: 4 },
             { name: 'Python', desktop: 2, mobile: 6 },
             { name: 'Ruby', desktop: 2, mobile: 6 },
             { name: 'Go', desktop: 1, mobile: 6 },
             { name: 'SQL', desktop: 2, mobile: 6 },
-            { name: 'C++', desktop: 2, mobile: 6 },
-            { name: 'C', desktop: 1, mobile: 6 },
-            { name: 'PHP', desktop: 2, mobile: 6 },
-            { name: 'Swift', desktop: 2, mobile: 6 },
+            { name: 'C++', desktop: 1, mobile: 5 },
+            { name: 'C', desktop: 1, mobile: 4 },
+            { name: 'PHP', desktop: 2, mobile: 5 },
+            { name: 'Kotlin', desktop: 2, mobile: 5 },
+            { name: 'Swift', desktop: 2, mobile: 5 },
         ],
         mobileNumColumns: [
             4,
             4,
-            4
+            5
         ]
     },
     tools: {
@@ -46,20 +47,22 @@ const SKILLS = {
         content: [
             { name: 'Node.js', desktop: 2, mobile: 5 },
             { name: 'React', desktop: 2, mobile: 5 },
-            { name: 'ASP.NET', desktop: 3, mobile: 5 },
+            { name: 'ASP.NET', desktop: 2, mobile: 5 },
             { name: 'Ruby on Rails', desktop: 4, mobile: 9 },
-            { name: 'Laravel', desktop: 2, mobile: 8 },
-            { name: 'Vue', desktop: 2, mobile: 8 },
-            { name: 'PyTorch', desktop: 2, mobile: 8 },
-            { name: 'Deno', desktop: 2, mobile: 6 },
-            { name: 'Jest', desktop: 1, mobile: 6 },
-            { name: 'NUnit', desktop: 2, mobile: 6 },
-            { name: 'JUnit', desktop: 2, mobile: 6 }
+            { name: 'Laravel', desktop: 2, mobile: 6 },
+            { name: 'Vue', desktop: 1, mobile: 6 },
+            { name: 'PyTorch', desktop: 2, mobile: 6 },
+            { name: 'Kafka', desktop: 2, mobile: 6 },
+            { name: 'Spark', desktop: 2, mobile: 5 },
+            { name: 'Deno', desktop: 2, mobile: 5 },
+            { name: 'Jest', desktop: 1, mobile: 4 },
+            { name: 'NUnit', desktop: 2, mobile: 5 },
+            { name: 'JUnit', desktop: 2, mobile: 5 }
         ],
         mobileNumColumns: [
             4,
-            3,
-            4
+            4,
+            5
         ]
     },
     nextId: 'experience'
